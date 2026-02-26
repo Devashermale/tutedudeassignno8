2b48917949b5b774946e3ec1feb20c0321364461
Devashermale <135997014+Devashermale@users.noreply.github.com>
2026-02-26 19:40:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: 'http://localhost:5173'}));
+  origin: 'https://tutedudeassignno8.vercel.app/'}));
 app.use('/',todo);
 
 
